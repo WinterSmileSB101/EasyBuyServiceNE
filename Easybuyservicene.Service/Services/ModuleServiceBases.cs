c01dc0b1fa62b68cd1591cc2ef9da688a552cd52
孝波 张 <zxb13194889618@outlook.com>
2017-12-23 16:58:28 +0800

    add DB SQL

---
+++ Easybuyservicene.Service/Services/ModuleServiceBases.cs
@@ -0,0 +1,17 @@
+﻿using Newegg.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easybuyservicene.Service.Services
+{
+    public class ModuleServiceBases<TRequest> : RestServiceBase<TRequest>
+    {
+        protected override void OnBeforeExecute(TRequest request)
+        {
+            RequestContext.HttpReq.ResponseContentType = "application/json";
+            RequestContext.HttpRes.Response.Charset = "utf-8";
+        }
+    }
+}
